c18b70cba864a367e3a5a3c1d52352ac4c68c8aa
santiagoSuarez219 <santiago8628@gmail.com>
2024-02-09 14:26:43 -0500

    Actualizar lang a "es" y agregar meta descripción en _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="es">
+      <Head>
+        <meta
+          name="description"
+          content="ECUTO App Repositorio Instituticional Colegio Mayor de Antioquia"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
