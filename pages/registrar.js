7f8ef928802d4edfb2b4c6ac8b7dfc26145ab870
santiagoSuarez219 <santiago8628@gmail.com>
2024-02-19 17:29:58 -0500

    Agrega formulario de registro y validación de campos

--- pages/registrar.js
+++ pages/registrar.js
@@ -1,7 +1,29 @@
+import { useState } from "react";
 import Link from "next/link";
 import Layout from "../layout/Layout";
+import Alerta from "../components/Alerta";
 
 export default function registrar() {
+  const [nombre, setNombre] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repetirPassword, setRepetirPassword] = useState("");
+  const [rol, setRol] = useState("");
+  const [alerta, setAlerta] = useState({});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if ([nombre, email, password, repetirPassword, rol].includes("")) {
+      setAlerta({
+        error: true,
+        msg: "Todos los campos son obligatorios",
+      });
+      return;
+    }
+  };
+
+  const { msg } = alerta;
+
   return (
     <Layout
       title={">Registrar"}
@@ -9,14 +31,20 @@ export default function registrar() {
         "Si eres investigador puedes registrar usuario en el sistema ECUOT"
       }
     >
-      <main className="w-full md:w-2/3 lg:w-11/12 mx-5 lg:flex items-center">
-        <h1 className="text-orange-500 font-black text-6xl capitalize lg:w-1/2">
-          Crea usuarios para administrar{" "}
-          <span className="text-slate-700">Ecuot</span>
-        </h1>
-        <div className="lg:w-1/2">
-          <form className="bg-white shadow rounded-lg px-10 py-5">
-            <div className="my-5">
+      <main className="w-full h-screen flex items-center px-6 gap-6">
+        <div className="w-1/2">
+          <h1 className="text-orange-500 font-black text-6xl capitalize">
+            Crea usuarios para administrar{" "}
+            <span className="text-slate-700">Ecuot</span>
+          </h1>
+          <div className="lg:w-full">{msg && <Alerta alerta={alerta} />}</div>
+        </div>
+        <div className="w-1/2">
+          <form
+            onSubmit={handleSubmit}
+            className="bg-white shadow rounded-lg w-full px-10 py-5"
+          >
+            <div className="">
               <label
                 htmlFor="nombre"
                 className="uppercase text-gray-600 block text-xl font-bold "
@@ -28,9 +56,10 @@ export default function registrar() {
                 className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
                 type="email"
                 placeholder="Nombre del usuario"
+                onChange={(e) => setNombre(e.target.value)}
               />
             </div>
-            <div className="my-5">
+            <div className="my-4">
               <label
                 htmlFor="email"
                 className="uppercase text-gray-600 block text-xl font-bold "
@@ -42,9 +71,10 @@ export default function registrar() {
                 className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
                 type="email"
                 placeholder="Email de registro"
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
-            <div className="my-5">
+            <div className="my-4">
               <label
                 htmlFor="password"
                 className="uppercase text-gray-600 block text-xl font-bold "
@@ -56,27 +86,49 @@ export default function registrar() {
                 className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
                 type="password"
                 placeholder="Contraseña de registro"
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
-            <div className="my-5">
+            <div className="my-4">
               <label
                 htmlFor="repetir-contraseña"
                 className="uppercase text-gray-600 block text-xl font-bold "
               >
-                Reperir contraseña
+                Repetir contraseña
               </label>
               <input
                 id="repetir-contraseña"
                 className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
                 type="password"
                 placeholder="Repetir la contraseña"
+                onChange={(e) => setRepetirPassword(e.target.value)}
               />
+            </div>
+            <div className="my-4">
+              <label
+                htmlFor="rol"
+                className="uppercase text-gray-600 block text-xl font-bold "
+              >
+                Selecciona un rol
+              </label>
+              <select
+                id="rol"
+                className="w-full mt-3 p-3 border rounded-xl bg-gray-50 text-gray-400"
+                onChange={(e) => setRol(e.target.value)}
+              >
+                <option value="investigador">Investigador</option>
+                <option value="estudiante">Estudiante</option>
+              </select>
             </div>
             <input
               type="submit"
               value={"Crear cuenta"}
-              className="w-full bg-orange-500 mb-5 py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-orange-600 transition-colors"
+              className="w-full bg-orange-500 py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-orange-600 transition-colors"
             />
+            {/* 
+            
+            
+             */}
           </form>
           <Link
             href="/"
