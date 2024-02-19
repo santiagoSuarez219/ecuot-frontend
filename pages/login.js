d9983df156024c19440396202bf42cba79be35aa
santiagoSuarez219 <santiago8628@gmail.com>
2024-02-19 17:07:32 -0500

    Actualizar diseño de página de inicio de sesión

--- pages/login.js
+++ pages/login.js
@@ -9,61 +9,59 @@ export default function Login() {
         "Pagina para ingresar credenciales y habilitar otras opciones"
       }
     >
-      <main className="mt-5 md:mt-20 w-full md:w-2/3 lg:w-2/5 mx-5">
-        <div className="">
-          <h1 className="text-orange-500 font-black text-6xl capitalize">
-            Inicia sesión
-          </h1>
-          <form className="my-10 bg-white shadow rounded-lg p-10">
-            <div className="my-5">
-              <label
-                htmlFor="email"
-                className="uppercase text-gray-600 block text-xl font-bold "
-              >
-                Email
-              </label>
-              <input
-                id="email"
-                className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
-                type="email"
-                placeholder="Email de registro"
-              />
-            </div>
-            <div className="my-5">
-              <label
-                htmlFor="password"
-                className="uppercase text-gray-600 block text-xl font-bold "
-              >
-                Password
-              </label>
-              <input
-                id="password"
-                className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
-                type="password"
-                placeholder="Password de registro"
-              />
-            </div>
+      <main className="w-full h-screen flex flex-col justify-center items-center ">
+        <h1 className="text-orange-500 font-black text-6xl capitalize">
+          Inicia sesión
+        </h1>
+        <form className="my-10 lg:w-2/5 bg-white shadow rounded-lg p-10">
+          <div className="my-5">
+            <label
+              htmlFor="email"
+              className="uppercase text-gray-600 block text-xl font-bold "
+            >
+              Email
+            </label>
             <input
-              type="submit"
-              value={"Iniciar Sesion"}
-              className="w-full bg-orange-500 mb-5 py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-orange-600 transition-colors"
+              id="email"
+              className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+              type="email"
+              placeholder="Email de registro"
             />
-          </form>
-          <nav className="lg:flex lg:justify-between">
-            <Link
-              href="/"
-              className="block text-center my-5 text-slate-500 uppercase"
+          </div>
+          <div className="my-5">
+            <label
+              htmlFor="password"
+              className="uppercase text-gray-600 block text-xl font-bold "
             >
-              Volver al inicio
-            </Link>
-            <Link
-              href="/olvide-password"
-              className="block text-center my-5 text-slate-500 uppercase text-sm"
-            >
-              Olvide mi contraseña
-            </Link>
-          </nav>
-        </div>
+              Password
+            </label>
+            <input
+              id="password"
+              className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+              type="password"
+              placeholder="Password de registro"
+            />
+          </div>
+          <input
+            type="submit"
+            value={"Iniciar Sesion"}
+            className="w-full bg-orange-500 mb-5 py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-orange-600 transition-colors"
+          />
+        </form>
+        <nav className="lg:w-2/5 lg:flex justify-between text-sm">
+          <Link
+            href="/"
+            className="block text-center my-5 text-slate-500 uppercase"
+          >
+            Volver al inicio
+          </Link>
+          <Link
+            href="/olvide-password"
+            className="block text-center my-5 text-slate-500 uppercase"
+          >
+            Olvide mi contraseña
+          </Link>
+        </nav>
       </main>
     </Layout>
   );
