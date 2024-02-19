2072b251bfc273cd1c41e6926763fdf4f0bdab18
santiagoSuarez219 <santiago8628@gmail.com>
2024-02-19 17:29:50 -0500

    Agregada página de recuperación de contraseña

--- pages/olvide-password.js
+++ pages/olvide-password.js
@@ -1,39 +1,46 @@
 import Link from "next/link";
-import Layout from "../../layout/Layout";
+import Layout from "../layout/Layout";
 
-export default function RecuperarContraseña() {
+export default function OlvideContraseña() {
   return (
     <Layout
-      title={">Registrar"}
+      title={"Login"}
       description={
-        "Si eres investigador puedes registrar usuario en el sistema ECUOT"
+        "Pagina para ingresar credenciales y habilitar otras opciones"
       }
     >
-      <main className="mt-5 md:mt-20 w-full md:w-2/3 lg:w-2/5 mx-5">
+      <main className="w-full h-screen flex flex-col justify-center items-center">
         <h1 className="text-orange-500 font-black text-6xl capitalize">
-          Reestablece tu contraseña
+          Recupera tu contraseña
         </h1>
-        <form className="my-10 bg-white shadow rounded-lg p-10">
+        <form className="lg:w-2/5 my-10 bg-white shadow rounded-lg p-10">
           <div className="my-5">
             <label
-              htmlFor="password"
+              htmlFor="email"
               className="uppercase text-gray-600 block text-xl font-bold "
             >
-              Contraseña
+              Email
             </label>
             <input
-              id="password"
+              id="email"
               className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
-              type="password"
-              placeholder="Nueva contraseña"
+              type="email"
+              placeholder="Email de registro"
             />
           </div>
+
           <input
             type="submit"
-            value={"Reestablecer contraseña"}
+            value={"Enviar instrucciones"}
             className="w-full bg-orange-500 mb-5 py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-orange-600 transition-colors"
           />
         </form>
+        <Link
+          href="/"
+          className="block text-center my-5 text-slate-500 uppercase"
+        >
+          Volver al inicio
+        </Link>
       </main>
     </Layout>
   );
