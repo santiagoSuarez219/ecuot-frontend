9185ca33b6a8373e2f5c4f90efbced04153a40ea
santiagoSuarez219 <santiago8628@gmail.com>
2024-02-20 08:10:08 -0500

    Agregado componente SobreNosotros.js

---
+++ components/SobreNosotros.js
@@ -0,0 +1,37 @@
+export default function SobreNosotros() {
+  return (
+    <section className="w-full flex flex-col lg:flex-row items-center md:px-6">
+      <h1 className="text-orange-500 font-black text-5xl md:text-6xl md:text-center lg:text-left uppercase w-full lg:w-1/2 p-4 md:p-0 md:mt-24">
+        Sobre nosotros
+      </h1>
+      <div className="w-full lg:w-1/2 lg:pl-12 p-4 md:p-6">
+        <p className="mt-6 text-gray-600 text-lg md:text-xl font-light text-justify">
+          ECOUT surge como resultado del proyecto de investigación “Conflictos
+          urbanos asociados a las intervenciones y decisiones referidas al
+          ordenamiento territorial de la ciudad de Medellín- Colombia” realizado
+          por el Grupo de Investigación en Estudios sobre Desarrollo Local y
+          Gestión Territorial de la Facultad de Ciencias Sociales y Educación de
+          la IUCMA. La investigación planteada en 2018 se trazó como objetivo
+          Caracterizar a través del análisis de prensa las tensiones en relación
+          con las decisiones del ordenamiento territorial en la ciudad de
+          Medellin entre 1916- 2016. Lográndose como resultado un sistema de
+          información que recupera y estructura la conflictividad asociada a los
+          procesos de ordenamiento territorial en relación con tres componentes:
+          Uno, Infonoticias, codifica la información identificada en prensa
+          sobre las tensiones urbanas puestas en el escenario público. Dos,
+          conflictividad. Caracteriza la conflictividad asociada a las
+          intervenciones físico espaciales y decisiones del ordenamiento
+          territorial. Tres, Intervenciones y decisiones, presenta las
+          características de las intervenciones y decisiones objeto de tensión
+          en el escenario público. El aplicativo ECUOT es desarrollado mediante
+          convenio con el grupo de investigación en Automática, Electrónica y
+          Ciencias Computacionales del ITM y tiene como propósito compartir los
+          hallazgos del estudio con la comunidad académica, constituyéndose en
+          una herramienta de consulta para orientarse en el reconocimiento de la
+          transversalidad de las tensiones inherentes a las decisiones e
+          intervenciones de ordenamiento territorial para la ciudad de Medellín.
+        </p>
+      </div>
+    </section>
+  );
+}
