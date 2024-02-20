54fe15c26ad0798b93a085fbab566b115e020351
santiagoSuarez219 <santiago8628@gmail.com>
2024-02-20 09:21:24 -0500

    version mobile seccion registrar usuarios

--- pages/registrar.js
+++ pages/registrar.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Link from "next/link";
+import axios from "axios";
 import Layout from "../layout/Layout";
 import Alerta from "../components/Alerta";
 
@@ -11,7 +12,7 @@ export default function registrar() {
   const [rol, setRol] = useState("usuario");
   const [alerta, setAlerta] = useState({});
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     if ([nombre, email, password, repetirPassword, rol].includes("")) {
       setAlerta({
@@ -27,29 +28,47 @@ export default function registrar() {
       });
       return;
     }
+    if (password.length < 6) {
+      setAlerta({
+        error: true,
+        msg: "La contraseña debe tener al menos 6 caracteres",
+      });
+      return;
+    }
+    try {
+      const respuesta = await axios.post("http://localhost:4000/api/usuarios", {
+        nombre,
+        email,
+        password,
+        rol,
+      });
+      console.log(respuesta);
+    } catch {}
   };
 
   const { msg } = alerta;
 
   return (
     <Layout
-      title={">Registrar"}
+      title={"Registrar"}
       description={
-        "Si eres investigador puedes registrar usuario en el sistema ECUOT"
+        "Si eres investigador puedes registrar usuarios en el sistema ECUOT"
       }
     >
-      <main className="w-full h-screen flex items-center px-6 gap-6">
-        <div className="w-1/2">
-          <h1 className="text-orange-500 font-black text-6xl capitalize">
+      <main className="w-full lg:h-screen flex flex-col lg:flex-row items-center md:px-6 md:gap-6">
+        <div className="lg:w-1/2">
+          <h1 className="p-4 md:p-6 md:text-center lg:text-left">
             Crea usuarios para administrar{" "}
             <span className="text-slate-700">Ecuot</span>
           </h1>
-          <div className="lg:w-full">{msg && <Alerta alerta={alerta} />}</div>
+          <div className="hidden lg:block">
+            {msg && <Alerta alerta={alerta} />}
+          </div>
         </div>
-        <div className="w-1/2">
+        <div className="lg:w-1/2 w-11/12">
           <form
             onSubmit={handleSubmit}
-            className="bg-white shadow rounded-lg w-full px-10 py-5"
+            className="bg-white shadow rounded-lg w-full md:px-10 px-4 py-5"
           >
             <div className="">
               <label
@@ -133,14 +152,11 @@ export default function registrar() {
               value={"Crear cuenta"}
               className="w-full bg-orange-500 py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-orange-600 transition-colors"
             />
-            {/* 
-            
-            
-             */}
           </form>
+          <div className="lg:hidden">{msg && <Alerta alerta={alerta} />}</div>
           <Link
             href="/"
-            className="block text-center text-slate-500 uppercase mt-5"
+            className="block text-center text-slate-500 uppercase my-5"
           >
             Volver al inicio
           </Link>
@@ -148,4 +164,14 @@ export default function registrar() {
       </main>
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  return {
+    props: {
+      title: "Registrar",
+      description:
+        "Si eres investigador puedes registrar usuario en el sistema ECUOT",
+    },
+  };
 }
