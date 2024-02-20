fe8dc3c4234cbe82d17bcdc20121de0d39675689
santiagoSuarez219 <santiago8628@gmail.com>
2024-02-20 08:10:01 -0500

    Actualizar estilos de Navbar en la versión móvil

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,16 +3,16 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="w-full flex justify-between p-6">
+    <nav className="w-full flex justify-center md:justify-between py-4 md:p-6">
       <h1 className="text-orange-500 font-black text-5xl">ECUOT</h1>
-      <div>
+      <div className="hidden md:block">
         <ul className="h-full flex gap-6 items-center text-xl font-medium">
           <li>Noticias</li>
           <li>Conflictividad</li>
           <li>Intervenciones</li>
         </ul>
       </div>
-      <div className="flex gap-6 items-center text-xl">
+      <div className="hidden md:flex gap-6 items-center text-xl">
         <p className="font-light">Invitado</p>
         <Link href="/login">Ingresar</Link>
       </div>
