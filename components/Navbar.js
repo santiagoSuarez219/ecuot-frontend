341fc714195c46272b009fad42013d98e0021661
santiagoSuarez219 <santiago8628@gmail.com>
2024-02-24 19:08:14 -0500

    Actualizar estilos y enlaces en la barra de navegación

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,19 +3,22 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="w-full flex justify-center md:justify-between py-4 md:p-6">
-      <h1 className="text-orange-500 font-black text-5xl">ECUOT</h1>
-      <div className="hidden md:block">
-        <ul className="h-full flex gap-6 items-center text-xl font-medium">
-          <li>Noticias</li>
-          <li>Conflictividad</li>
-          <li>Intervenciones</li>
-        </ul>
+    <nav className="w-full md:flex lg:px-8 md:mb-6 md:justify-center lg:justify-between md:items-center md:gap-8">
+      <div className="md:flex lg:gap-8">
+        <h1 className="font-bold text-2xl lg:text-3xl tracking-tighter w-full text-center md:max-w-[200px] lg:max-w-screen-lg">
+          ECUOT Repositorio Institucional
+        </h1>
+        <div className="hidden md:block">
+          <ul className="h-full flex lg:gap-8 gap-4 items-center text-sm font-medium">
+            <li>Noticias</li>
+            <li>Conflictividad</li>
+            <li>Intervenciones</li>
+          </ul>
+        </div>
       </div>
-      <div className="hidden md:flex gap-6 items-center text-xl">
-        <p className="font-light">Invitado</p>
-        <Link href="/login">Ingresar</Link>
-      </div>
+      <button className="hidden md:block p-2 border border-gray-400 rounded-lg h-1/2 ">
+        <Link href="/login">Iniciar sesion</Link>
+      </button>
     </nav>
   );
 }
