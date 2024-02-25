c49f1d0409df120d5960dd007794c383ec4e1c12
santiagoSuarez219 <santiago8628@gmail.com>
2024-02-24 19:10:05 -0500

    Actualizar el color de fondo en Layout.jsx

--- layout/Layout.jsx
+++ layout/Layout.jsx
@@ -7,7 +7,7 @@ export default function Layout({ children, title = "", description = "" }) {
         <title>{`Ecuot - ${title}`}</title>
         <meta name="description" content={description} />
       </Head>
-      <div className="w-full min-h-screen flex flex-col md:block bg-gray-100 lg:p-6 p-4">
+      <div className="w-full min-h-screen flex flex-col md:block bg-white lg:p-6 p-4">
         {children}
       </div>
     </>
