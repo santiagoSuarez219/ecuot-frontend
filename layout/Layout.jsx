f06fe7d218c6ee5775bfd65575e24be4fe20d675
santiagoSuarez219 <santiago8628@gmail.com>
2024-03-04 06:49:32 -0500

    Añadir Navbar y useEcuot a Layout.jsx

--- layout/Layout.jsx
+++ layout/Layout.jsx
@@ -1,13 +1,18 @@
 import Head from "next/head";
+import Navbar from "../components/Navbar";
+import useEcuot from "../hooks/useEcuot";
 
 export default function Layout({ children, title = "", description = "" }) {
+  const { modal } = useEcuot();
+
   return (
     <>
       <Head>
         <title>{`Ecuot - ${title}`}</title>
         <meta name="description" content={description} />
       </Head>
-      <div className="w-full min-h-screen flex flex-col md:block bg-white lg:p-6 p-4">
+      <div className="w-full min-h-screen flex flex-col md:block bg-white">
+        <Navbar />
         {children}
       </div>
     </>
