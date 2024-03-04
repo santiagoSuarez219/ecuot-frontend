e4b4913c9c873a1eb7f6546aa58104b2eb25c0ae
santiagoSuarez219 <santiago8628@gmail.com>
2024-03-04 06:49:10 -0500

    Agregado menú desplegable y modal en la barra de navegación

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,24 +1,79 @@
-import Image from "next/image";
 import Link from "next/link";
+import Modal from "react-modal";
+import { HiBars3 } from "react-icons/hi2";
+import { HiOutlineXMark } from "react-icons/hi2";
+import useEcuot from "../hooks/useEcuot";
+
+const customStyles = {
+  content: {
+    top: "auto",
+    left: "auto",
+    right: 0,
+    bottom: 0,
+    width: "70%",
+    height: "calc(100vh - 96px)",
+    overflow: "auto",
+    padding: "20px",
+    transform: "translateX(0)",
+  },
+};
+
+Modal.setAppElement("#__next");
 
 export default function Navbar() {
+  const { handleOpenModal, handleCloseModal, modalNavbar } = useEcuot();
   return (
-    <nav className="w-full md:flex lg:px-8 md:mb-6 md:justify-center lg:justify-between md:items-center md:gap-8">
-      <div className="md:flex lg:gap-8">
-        <h1 className="font-bold text-2xl lg:text-3xl tracking-tighter w-full text-center md:max-w-[200px] lg:max-w-screen-lg">
-          ECUOT Repositorio Institucional
-        </h1>
-        <div className="hidden md:block">
-          <ul className="h-full flex lg:gap-8 gap-4 items-center text-sm font-medium">
-            <li>Noticias</li>
-            <li>Conflictividad</li>
-            <li>Intervenciones</li>
+    <>
+      <nav className="w-full flex p-4 lg:p-6 items-center justify-between">
+        <div className="flex items-center">
+          <h1 className="font-bold text-2xl md:text-3xl lg:text-4xl tracking-tighter lg:mr-8">
+            <Link href={"/"}>ECUOT Repositorio Institucional</Link>
+          </h1>
+          <ul className="hidden lg:flex gap-6 text-sm font-medium">
+            <li className="hover:border-b-2">
+              <Link href={"/noticias"}>Noticias</Link>
+            </li>
+            <li className="hover:border-b-2">
+              <Link href={"/conflictos"}>Conflictos</Link>
+            </li>
+
+            <li className="hover:border-b-2">
+              <Link href={"/intervenciones"}>Intervenciones</Link>
+            </li>
           </ul>
         </div>
-      </div>
-      <button className="hidden md:block p-2 border border-gray-400 rounded-lg h-1/2 ">
-        <Link href="/login">Iniciar sesion</Link>
-      </button>
-    </nav>
+        <button className="hidden lg:block p-2 border border-gray-400 rounded-lg hover:bg-gray-200 transition-colors">
+          <Link href="/login">Iniciar sesion</Link>
+        </button>
+        {modalNavbar ? (
+          <HiOutlineXMark
+            className="text-5xl z-10 lg:hidden"
+            onClick={handleCloseModal}
+          />
+        ) : (
+          <HiBars3
+            className="text-5xl z-10 lg:hidden"
+            onClick={handleOpenModal}
+          />
+        )}
+        {modalNavbar && (
+          <Modal isOpen={modalNavbar} style={customStyles}>
+            <ul className="flex flex-col gap-2 font-semibold">
+              <li onClick={handleCloseModal}>
+                <Link href={"/noticias"}>Noticias</Link>
+              </li>
+              <li onClick={handleCloseModal}>
+                <Link href={"/conflictos"}>Conflictos</Link>
+              </li>
+              <li onClick={handleCloseModal}>
+                <Link href={"/intervenciones"}>Intervenciones</Link>
+              </li>
+              <div className="w-full h-[1px] rounded-sm bg-slate-200"></div>
+              <li>Iniciar sesion</li>
+            </ul>
+          </Modal>
+        )}
+      </nav>
+    </>
   );
 }
