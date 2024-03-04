abea001db1c18af3532cbf52f718679d337ee83a
santiagoSuarez219 <santiago8628@gmail.com>
2024-03-04 06:48:59 -0500

    Agregado manejo de modal en EcuotProvider.jsx

--- context/EcuotProvider.jsx
+++ context/EcuotProvider.jsx
@@ -3,7 +3,27 @@ import { useState, createContext } from "react";
 const EcuotContext = createContext();
 
 const EcuotProvider = ({ children }) => {
-  return <EcuotContext.Provider value={{}}>{children}</EcuotContext.Provider>;
+  const [modalNavbar, setModalNavbar] = useState(false);
+
+  const handleOpenModal = () => {
+    setModalNavbar(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalNavbar(false);
+  };
+
+  return (
+    <EcuotContext.Provider
+      value={{
+        modalNavbar,
+        handleOpenModal,
+        handleCloseModal,
+      }}
+    >
+      {children}
+    </EcuotContext.Provider>
+  );
 };
 
 export { EcuotProvider };
