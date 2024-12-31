04931ac6fd462d68feddc8c46847a4a520390328
santiagoSuarez219 <santiago8628@gmail.com>
2024-12-31 15:34:14 -0500

    mejora el diseño de LoginView: agrega un fondo de imagen y actualiza estilos de botones y texto

--- src/views/Auth/LoginView/index.tsx
+++ src/views/Auth/LoginView/index.tsx
@@ -6,6 +6,7 @@ import { toast } from "react-toastify";
 import LoginForm from "../../../components/Auth/LoginForm";
 import { login } from "../../../api/AuthAPI";
 import { LoginType } from "../../../types";
+import { IoReturnUpBack } from "react-icons/io5";
 
 export default function LoginView() {
   const navigate = useNavigate();
@@ -36,18 +37,27 @@ export default function LoginView() {
 
   const handleForm = (formData: LoginType) => mutate(formData);
   return (
-    <main className="w-full h-screen flex px-4 md:px-6 lg:px-0 justify-center items-center relative bg-gradient-to-r from-quaternary ">
+    <main className="w-full h-screen flex px-4 md:px-6 lg:px-0 justify-center items-center relative  ">
+      <figure className="absolute inset-0 -z-10">
+        <img
+          src="/public/sistema_movilidad.avif"
+          alt="main-poster"
+          className="w-full h-full object-cover"
+        />
+      </figure>
+      {/* <div className="absolute inset-0 bg-primary opacity-70 -z-10"></div> */}
       <Link
         to="/"
-        className="absolute top-6 left-6 bg-primary text-white rounded-xl px-6 py-4 uppercase font-bold hover:bg-secondary transition-all"
+        className="absolute top-6 left-6 bg-quinary text-white rounded-xl px-6 py-4 uppercase font-bold hover:scale-105 transition-all flex items-center gap-2"
       >
+        <IoReturnUpBack className="size-7" />
         Volver al home
       </Link>
-      <div className="w-full max-w-screen-md rounded-2xl shadow-xl p-4 md:p-16 bg-white">
+      <div className="w-full max-w-screen-md rounded-2xl shadow-xl p-4 md:p-16 bg-white text-septenary">
         <h3 className="text-3xl md:text-5xl font-bold mt-5 mb-2 text-primary text-left">
           Iniciar sesion
         </h3>
-        <p className="text-font-color mb-5 text-left">
+        <p className=" mb-5 text-left">
           Introduzca su dirección de correo electrónico para acceder a su cuenta
         </p>
         <form
@@ -59,7 +69,7 @@ export default function LoginView() {
           <input
             type="submit"
             value={"Iniciar Sesion"}
-            className="w-full bg-primary mb-5 py-3 text-white text-xl rounded cursor-pointer hover:bg-secondary transition-colors"
+            className="w-full bg-quinary font-semibold uppercase mb-5 py-3 text-white text-xl rounded cursor-pointer hover:scale-105 transition-all"
           />
         </form>
       </div>
